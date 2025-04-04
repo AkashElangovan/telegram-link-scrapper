7084e93138ded9c135b4adea285dd4e1354f953a
Akash Elangovan <67691796+AkashElangovan@users.noreply.github.com>
2025-04-04 21:47:09 +0530

    Add files via upload

---
+++ img scrapper.py
@@ -0,0 +1,45 @@
+import requests
+from bs4 import BeautifulSoup
+import os
+from concurrent.futures import ThreadPoolExecutor
+
+def download_image(url, output_folder):
+    try:
+        img_response = requests.get(url)
+        if img_response.status_code == 200:
+            img_name = url.split('/')[-1]
+            with open(os.path.join(output_folder, img_name), 'wb') as img_file:
+                img_file.write(img_response.content)
+            print(f"Image '{img_name}' downloaded from '{url}'")
+    except Exception as e:
+        print(f"Error downloading image from '{url}': {e}")
+
+def scrape_images_from_urls(input_file, output_folder):
+    # Create the output folder if it doesn't exist
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
+
+    # Read URLs from the input file
+    with open(input_file, 'r') as f:
+        urls = f.readlines()
+
+    # Download images concurrently using ThreadPoolExecutor
+    with ThreadPoolExecutor(max_workers=10) as executor:
+        for url in urls:
+            url = url.strip()
+            response = requests.get(url)
+            if response.status_code == 200:
+                soup = BeautifulSoup(response.content, 'html.parser')
+                img_tags = soup.find_all('img')
+                for img_tag in img_tags:
+                    img_url = img_tag.get('src')
+                    if img_url:
+                        executor.submit(download_image, img_url, output_folder)
+
+def main():
+    input_file = input("Enter the path of the input file: ")
+    output_folder = r"C:\Users\akash\OneDrive\Desktop\scrapper\images"  # Output folder to save images
+    scrape_images_from_urls(input_file, output_folder)
+
+if __name__ == "__main__":
+    main()
---
+++ scrapper.py
@@ -0,0 +1,21 @@
+import re
+
+def find_tradingview_links(input_file, output_file):
+    # Open the input file with UTF-8 encoding
+    with open(input_file, 'r', encoding='utf-8') as f:
+        text = f.read()
+    
+    # Find all links related to https://www.tradingview.com/
+    links = re.findall(r'https://www\.tradingview\.com/[^\s"]+', text)
+
+    # Write the links into a new text file
+    with open(output_file, 'w') as f:
+        for link in links:
+            f.write(link + '\n')
+
+    print("Links related to 'https://www.tradingview.com/' have been extracted and saved to", output_file)
+
+# Example usage:
+input_file = r"C:\Users\akash\OneDrive\Desktop\scrapper\input_text.txt"  # Provide the path to your input text file
+output_file = r"C:\Users\akash\OneDrive\Desktop\scrapper\tradingview_links.txt"  # Provide the path where you want to save the output
+find_tradingview_links(input_file, output_file)
